6209aee9015d9b4a1dddea59cdb664252291da96
INGUI54 <96308047+INGUI54@users.noreply.github.com>
2023-01-01 20:45:13 +0700

    Update Send_Email_RDCOMClient.R

--- Send_Email_RDCOMClient.R
+++ Send_Email_RDCOMClient.R
@@ -1,15 +1,51 @@
-library(RDCOMClient)
+# Load the xlsx package
+library("xlsx")
 
-OutApp<-COMCreate("Outlook.Application")
+# Create an empty Excel workbook
+workbook <- createWorkbook(type="xlsx")
 
-outMail=OutApp$CreateItem(0)
+# Add a format for the title
+titleStyle <- CellStyle(workbook) + Font(workbook, name = "Calibri Light", heightInPoints=18, color="#AE4371")
 
-outMail[["To"]]=paste("duy.nguyen03@easycredit.vn","duy.nguyen03@easycredit.vn",sep=";",collapse = NULL)
-outMail[["Cc"]]=paste("","",sep=";",collapse = NULL)
-outMail[["subject"]]=paste("OCR-eKYC: Số lượng request eKYC truy vấn tháng ",paste(format(Sys.Date()-27,format="%Y"),format(Sys.Date()-27,format="%m"),sep=""),sep="")
-outMail[["HTMLbody"]]=paste("<p>","Dear BI,<br>","FIN gửi số lượng request eKYC ghi nhận. Nhờ BI hỗ trợ check và phản hồi sớm","</p>",paste0("Thanks & Regards,<br>","Nguyễn Đức Duy"))
+# Add a format for table headings
+headingStyle <- CellStyle(workbook) + Font(workbook, name = "Calibri", heightInPoints=11, color="#44546A",  isBold=TRUE) + Border(color="#8EA9DB", position="BOTTOM", pen="BORDER_MEDIUM")
 
-  
-#outMail[["attachments"]]$Add(paste("E:/Reconliiation/RISK_2/Reconsile/",paste(format(Sys.Date()-27,format="%Y"),format(Sys.Date()-27,format="%m"),sep=""),"/eKYC","/","GIAO_DICH_OCR_BI_KHONG_GHI_NHAN_",paste(format(Sys.Date()-27,format="%Y"),format(Sys.Date()-27,format="%m"),sep=""),".xlsx",sep=""))
-outMail$Display()
-outMail$Send()
+# Add a format for table rows
+rowStyle <- CellStyle(workbook) + Font(workbook, name = "Calibri", heightInPoints=11, isBold=TRUE)
+
+# Add a format for comments
+commentStyle <- CellStyle(workbook) + Font( workbook, name = "Calibri", heightInPoints=11, color="#7F7F7F", isItalic=TRUE)
+
+# Add a sheet to our workbook
+sheet1 <- createSheet(workbook, sheetName = "4 Cylinder Cars")
+
+# We'll start with adding a title
+
+# First we'll create a new row, let's put the title on row 2
+titleRow <-createRow(sheet1, rowIndex=2)
+# We'll add a cell to that row (in the first column) for the title
+sheetTitle <-createCell(titleRow, colIndex=1)
+# We'll add the sheet title to the cell
+setCellValue(sheetTitle[[1,1]], "4 Cylinder Cars")
+# We'll choose the formatting for the cell
+setCellStyle(sheetTitle[[1,1]], titleStyle)
+
+# Now we'll repeat this process to add a comment to the worksheet
+
+# First we'll create a new row, let's put the comment on row 4
+titleRow <-createRow(sheet1, rowIndex=4)
+# We'll add a cell to that row (in the first column) for the comment
+sheetTitle <-createCell(titleRow, colIndex=1)
+# We'll add the comment to the cell
+setCellValue(sheetTitle[[1,1]], "A listing of all the 4 cylinder cars from the mtcars dataset")
+# We'll choose the formatting for the cell
+setCellStyle(sheetTitle[[1,1]], commentStyle)
+
+# Add a data frame to our worksheet
+addDataFrame(mtcars[mtcars$cyl == 4 , ], sheet1, startRow=6, startColumn=1, colnamesStyle = headingStyle, rownamesStyle = rowStyle)
+
+# Set the width of the first column to be wider so the full car names display
+setColumnWidth(sheet1, colIndex=1, colWidth=15)
+
+# Save the workbook
+saveWorkbook(workbook, "workbook.xlsx")
