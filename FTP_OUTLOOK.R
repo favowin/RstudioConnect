b0503ba16576b3d69fcb707481f5d1d4ab3eb02d
INGUI54 <96308047+INGUI54@users.noreply.github.com>
2023-01-02 12:30:23 +0700

    Update FTP_OUTLOOK.R

--- FTP_OUTLOOK.R
+++ FTP_OUTLOOK.R
@@ -1,43 +1,9 @@
 library(RDCOMClient)
 library(openxlsx)
 library(lubridate)
-
-# setwd("E:/Taiwanese")
-outlook_app <- COMCreate("Outlook.Application")
-search <- outlook_app$AdvancedSearch(
-  "Inbox",
-  "urn:schemas:httpmail:subject like '%EPAY_EVN_%'"
-)
-
-Sys.sleep(10)
-results <- search$Results()
-attachment_file <- tempfile()
-
 library(RCurl)
-
-date <- function(){
-  if ((wday(format(Sys.Date(), "%Y-%m-%d"), label = FALSE)) == 1){
-    return(format(Sys.Date(), "%Y-%m-%d"))
-  } else {
-    return(format(Sys.Date()-1, "%Y-%m-%d"))
-  }
-}
-
-for (i in 400:results$Count()) {
-  if (as.Date("1899-12-30") + floor(results$Item(i)$ReceivedTime()) == as.Date(date())) {
-    email <- results$Item(i)
-    attachment <- email$Attachments()
-    
-    for(j in 1:attachment$Count()) {
-      if (grepl(".xlsx", attachment$Item(j)$FileName(), ignore.case = TRUE)) {
-        attachmentname <- attachment$Item(j)$FileName()
-        attachment_file <- paste0(getwd(), "/", attachmentname)
-        # attachment$Item(j)$SaveAsFile(attachment_file)
-        # ftpUpload(attachment$Item(j)$FileName(), paste("ftp://ngejmair:Fofawubian369@ftp.fofawubian369.com/public_html/wp-content/uploads/2022/11/",gsub(".xlsx",".xlsx",attachment$Item(j)$FileName()),sep=""))
-        ftpUpload(attachment$Item(j)$FileName(), paste("ftp://ngejmair:Fofawubian369@ftp.fofawubian369.com/public_html/wp-content/uploads/2022/11/",attachment$Item(j)$FileName(),sep=""))
-        
-      }
-      Sys.sleep(10)
-    }
-  }
-}
+Day<-format(Sys.Date()-1,format="%d")
+Month<-format(Sys.Date()-1,format="%m")
+Year<-format(Sys.Date()-1,format="%Y")
+Date<-paste(Year,Month,Day,sep="")
+ftpUpload(paste("data/Payment_Collection_CASHLOAN_",Date,".csv",sep=""), paste("ftp://ngejmair:Fofawubian369@ftp.fofawubian369.com/public_html/wp-content/uploads/2022/11/",paste("data/Payment_Collection_CASHLOAN_",Date,".csv",sep=""),sep=""))
